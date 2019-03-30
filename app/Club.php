345ddab110cbe3df236c895e876149d3143e99e3
Martin Kagume <martiniriga@gmail.com>
2019-03-30 11:03:07 +0300

    image of club

--- app/Club.php
+++ app/Club.php
@@ -8,17 +8,22 @@ class Club extends Model
 {
     protected $fillable = [
         'name', 'email', 'phone', 'website',
-        'postal_address', 'physical_address','latlong'
+        'postal_address', 'physical_address','latlong','photo_url'
     ];
     public function users(){
         return $this->belongsToMany(User::Class)->withPivot('user_id','member_no');
     }
 
     public function games(){
-        return $this->hasMany('App/Games');
+        return $this->hasMany(Game::class);
     }
 
     public function rates(){
-        return $this->hasMany('App/Rate');
+        return $this->hasMany(Rate::Class);
+    }
+
+    public function getPhotoUrlAttribute($value){
+        $default_avatar = "img/business-avatar.png";
+        return $value ? url($value) :env('APP_URL')."/".($default_avatar);
     }
 }
